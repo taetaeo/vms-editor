83c602003aa3dd5bdb42d23b9fd28b3a2213a455
ohtaekwon <ohtk1119@gmail.com>
2024-07-18 23:22:48 +0900

    feat: Add Text aligns

--- package/src/views/index.ts
+++ package/src/views/index.ts
@@ -1,1 +1,2 @@
 export { default as EditorView } from "./editor.view";
+export { default as ToolbarView } from "./toolbar.view";
--- package/src/views/toolbar.view.tsx
+++ package/src/views/toolbar.view.tsx
@@ -1,28 +1,74 @@
-// Functions
-import { useToolbarCtxHandler, useVideoCtxHandler, useCanvasCtxHandler } from "../shared/handlers";
+import * as R from "react";
+import cn from "classnames";
+// Handlers
+import {
+  useToolbarCtxHandler,
+  useVideoCtxHandler,
+  useCanvasCtxHandler,
+} from "../shared/handlers";
 
 import { ToolbarModel } from "../models";
 
-const ToolbarView = () => {
+interface Props
+  extends R.HtmlHTMLAttributes<HTMLElement>,
+    R.PropsWithChildren {}
+
+const ToolbarView: R.FC<Props> = ({ style, children, ...rest }) => {
   const videoCtx = useVideoCtxHandler();
   const canvasCtx = useCanvasCtxHandler();
   const toolbarCtx = useToolbarCtxHandler();
 
-  const viewModel = new ToolbarModel(videoCtx, canvasCtx, toolbarCtx);
+  const toolbarModel = new ToolbarModel(videoCtx, canvasCtx, toolbarCtx);
 
   return (
     <div>
-      <button id="type" value="textBox" onClick={viewModel.handleChangeObjectType}>
+      {/* 객체 추가 버튼 Start */}
+      <button
+        id="type"
+        value="textBox"
+        onClick={toolbarModel.handleChangeObjectType}
+      >
         텍스트 추가하기
       </button>
-      <button id="type" value="image" onClick={viewModel.handleChangeObjectType}>
+      <button
+        id="type"
+        value="image"
+        onClick={toolbarModel.handleChangeObjectType}
+      >
         이미지 추가하기
       </button>
-      <button id="type" value="video" onClick={viewModel.handleChangeObjectType}>
+      <button
+        id="type"
+        value="video"
+        onClick={toolbarModel.handleChangeObjectType}
+      >
         동영상 추가하기
       </button>
+      {/* 객체 추가 버튼 End */}
 
-      <button></button>
+      {/* 정렬 스타일 Start*/}
+      <button
+        id="font_align"
+        value="left"
+        onClick={toolbarModel.handleChangeFontAlign}
+      >
+        왼쪽 정렬
+      </button>
+      <button
+        id="font_align"
+        value="center"
+        onClick={toolbarModel.handleChangeFontAlign}
+      >
+        가운데 정렬
+      </button>
+      <button
+        id="font_align"
+        value="right"
+        onClick={toolbarModel.handleChangeFontAlign}
+      >
+        오른쪽 정렬
+      </button>
+      {/* 정렬 스타일 End */}
     </div>
   );
 };
